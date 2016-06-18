40c13dee1044ac89c97cf9ede0ce63f55611dcad
Andi Devois <devoisaurus@MacBook-Pro.local>
2016-06-18 13:48:01 -0500

    before changing code

--- app/factories/StarterCards.js
+++ app/factories/StarterCards.js
@@ -4,7 +4,7 @@ app.factory("StarterCards", function($q, $http){
 	var getCards = function(activities) {
 		var activities = [];
 			return $q(function(resolve, reject){
-				$http.get("../data/activities.json")
+				$http.get("https://cmon-get-happy.firebaseio.com/activities.json")
 				.success(function(data){
 			 resolve(data);
 				}, function(error){
